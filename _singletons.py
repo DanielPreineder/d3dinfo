49b8069cbd0be19eb081af4f4ee09f228bfaf30a
filipppavlov <filipppavlov@users.noreply.github.com>
2017-10-11 14:55:07 +0000

    Add Aftermath to Trinity https://jira.ccpgames.com/browse/GFX-3493
    Added logging for removed devices
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/packages/trinity/": change = 1198250]

--- _singletons.py
+++ _singletons.py
@@ -2,6 +2,7 @@
 This is where we initialize singletons that are accessible at module root level.
 e.g. trinity.SingletonName
 """
+import logging
 
 import blue
 
@@ -21,3 +22,10 @@ renderJobs = renderjobs.RenderJobs()
 
 from . import GraphManager
 graphs = GraphManager.GraphManager()
+
+
+def _ReportRemovedDevice(hr, message, count, marker):
+    logging.error('GPU device removed',
+                  extra={"count": count, "error_message": message, "tags": {"reason": '0x%x' % hr, "marker": marker}})
+
+device.onDeviceRemoved = _ReportRemovedDevice
